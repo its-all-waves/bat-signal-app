9a8a045a844d49950fcd1b43a99d2778ed6786d0
kalypsohoman <catcatmcgee@gmail.com>
2025-05-25 18:02:32 -0500

    edit server.ts

--- server.ts
+++ server.ts
@@ -1,99 +1,46 @@
 /**
-
 Serve dingDong.
-
-TODO:
-  FEATURE:
-    - [ ] tell user when the bat signal is unavailable
-
 */
 
-import { SSESink } from "jsr:@planigale/sse";
 import BatSignal from "./BatSignal.ts";
-
-const HOSTNAME = "0.0.0.0";
-const PORT = 8080;
-
-const SSE_MSG_INTERVAL_MS = 500;
+import { allowedOrigins } from "./secrets.ts";
 
 const batSignal = new BatSignal();
 await batSignal.connect(); // TODO: handle error -- cannot connect -- exponential backoff
 
-Deno.serve({ hostname: HOSTNAME, port: PORT }, async (req) => {
-  const { method, url } = req;
-  const { pathname } = new URL(url);
-  console.log(
-    `${new Date().toISOString()} :: Request :: ${method} :: ${pathname}`,
-  );
+
+Deno.serve((req) => {
+  const { pathname } = new URL(req.url);
+  const origin = req.headers.get("origin")
+
+  if (!origin) return serverError()
+
+  const isCool = authenticate(req)
+  if (!isCool) {
+    return unauthorizedResponse()
+  }
 
   switch (pathname) {
-    case "/":
-      try {
-        const index = await Deno.open("./static/index.html", { read: true });
-        return new Response(index.readable);
-      } catch {
-        return notFoundResponse();
-      }
-
-    case "/static/index.js":
-      try {
-        const indexJs = await Deno.open("./static/index.js", { read: true });
-        return new Response(indexJs.readable, {
-          headers: { "Content-Type": "text/javascript" },
-        });
-      } catch {
-        return notFoundResponse();
-      }
-
     case "/connect": {
-      if (method !== "POST") return unauthorizedResponse();
-      try {
-        const { isAuthorizedLOL } = await req.json();
-        if (!isAuthorizedLOL) return unauthorizedResponse();
-      } catch {
-        return unauthorizedResponse();
-      }
-      const sseSink = new SSESink();
-      let interval = null;
-      try {
-        interval = setInterval(() => {
-          try {
-            sseSink.sendMessage({
-              data: JSON.stringify({
-                sse_interval_ms: SSE_MSG_INTERVAL_MS,
-                is_bat_signal_busy: batSignal.isOn(),
-                is_someone_coming: batSignal.isSomeoneComing(),
-              }),
-            });
-          } catch (err) {
-            // console.error("ERROR: Why does this happen?", err);
-          }
-        }, SSE_MSG_INTERVAL_MS);
-        return sseSink.toResponse(); // TODO: what is this response? check in front end
-      } catch (err) {
-        console.error("ERROR: Issue with SSE:", err);
-      }
-      interval && clearInterval(interval);
-      break;
+      const stream = getStream(req)
+
+      return new Response(stream, {
+        headers: {
+          "Content-Type": "text/event-stream",
+          "Cache-Control": "no-cache",
+          "Connection": "keep-alive",
+          "Access-Control-Allow-Origin": origin,
+        }
+      });
     }
 
-    // avoid responding to headless, automated requests by
-    // requiring POST and body: { `isAuthorizedLol` }
     case "/dingDong": {
-      if (method !== "POST") return unauthorizedResponse();
-      try {
-        const { isAuthorizedLOL } = await req.json();
-        if (!isAuthorizedLOL) return unauthorizedResponse();
-      } catch {
-        return unauthorizedResponse();
-      }
-
       try {
         batSignal.on();
-        return Response.json({ success: true });
+        return Response.json({success: true})
       } catch (err) {
-        console.error(err);
-        return Response.json({ success: false });
+        console.log("Dingdong endpoint caused exception. Error: ", err);
+        return Response.json({ success: false})
       }
     }
   }
@@ -101,10 +48,48 @@ Deno.serve({ hostname: HOSTNAME, port: PORT }, async (req) => {
   return notFoundResponse();
 });
 
+async function authenticate(req: Request) {
+  if (req.method !== "POST") return false;
+  
+  const origin = req.headers.get("origin")
+  if (!origin) return false
+  if (!allowedOrigins.includes(origin)) return false;
+  
+  const { isAuthorizedLOL } =  await req.json();
+  if (!isAuthorizedLOL) return false;
+
+  return true;
+}
+
+function getStream(req: Request) {
+  const stream = new ReadableStream({
+          start(controller) {
+            const encoder = new TextEncoder();
+            const interval = setInterval(() => {
+              const data = `data: ${JSON.stringify({
+                is_bat_signal_busy: batSignal.isOn(),
+                is_someone_coming: batSignal.isSomeoneComing(),
+                })}\n\n`;
+              controller.enqueue(encoder.encode(data));
+            }, 1000);
+
+            req.signal.addEventListener("abort", () => {
+              clearInterval(interval);
+              controller.close();
+            });
+          },
+        });
+  return stream
+}
+
 function notFoundResponse() {
   return new Response("404 Not Found", { status: 404 });
 }
 
 function unauthorizedResponse() {
-  return new Response("Unathorized", { status: 401 });
+  return new Response("Unauthorized", { status: 401 });
+}
+
+function serverError() {
+  return new Response("Oops, server error", { status: 501})
 }
