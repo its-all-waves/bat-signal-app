c152368a14b235259b758274cf3e69c082fa9966
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2025-05-26 17:40:52 -0500

    fix: restore missing code -- likely removed for testing; factor out headers; move auth to only /connect and /dingDong

--- server.ts
+++ server.ts
@@ -1,55 +1,82 @@
 /**
+
 Serve dingDong.
+
+TODO:
+  FEATURE:
+    - [ ] tell user when the bat signal is unavailable
+
 */
 
 import BatSignal from "./BatSignal.ts";
 import { allowedOrigins } from "./secrets.ts";
 
+// const HOSTNAME = "0.0.0.0"; // for Docker deployment
+const HOSTNAME = "localhost";
+const PORT = 8080;
+
+const SSE_MSG_INTERVAL_MS = 500;
+
 const batSignal = new BatSignal();
 await batSignal.connect(); // TODO: handle error -- cannot connect -- exponential backoff
 
+Deno.serve({ hostname: HOSTNAME, port: PORT }, async (req: Request) => {
+  const { method, url } = req;
+  const { pathname } = new URL(url);
 
-Deno.serve((req) => {
-  const { pathname } = new URL(req.url);
-  const origin = req.headers.get("origin")
+  const origin = req.headers.get("origin");
 
-  if (!origin) return serverError()
-
-  const isCool = authenticate(req)
-  if (!isCool) {
-    return unauthorizedResponse()
-  }
+  console.log(
+    `[ REQ ] ${new Date().toISOString()} :: ${origin ?? "[ NO ORIGIN ]"} :: ${method} :: ${pathname}`,
+  );
 
   switch (pathname) {
-    case "/connect": {
-      const stream = getStream(req)
+    case "/":
+      try {
+        const index = await Deno.open("./static/index.html", { read: true });
+        return new Response(index.readable);
+      } catch {
+        return notFoundResponse();
+      }
 
-      return new Response(stream, {
-        headers: {
-          "Content-Type": "text/event-stream",
-          "Cache-Control": "no-cache",
-          "Connection": "keep-alive",
-          "Access-Control-Allow-Origin": origin,
-        }
-      });
+    case "/static/index.js":
+      try {
+        const indexJs = await Deno.open("./static/index.js", { read: true });
+        return new Response(indexJs.readable, {
+          headers: { "Content-Type": "text/javascript" },
+        });
+      } catch {
+        return notFoundResponse();
+      }
+
+    case "/connect": {
+      const isCool = await authenticate(req);
+      if (!isCool) {
+        return unauthorizedResponse();
+      }
+      const stream = getStream(req);
+      return new Response(stream, { headers: sseHeaders(origin!) });
     }
 
     case "/dingDong": {
+      const isCool = await authenticate(req);
+      if (!isCool) {
+        return unauthorizedResponse();
+      }
+
       try {
         batSignal.on();
-        console.log(`ding dong at ${new Date()}`)
+        console.log(`ding dong at ${new Date()}`);
         return Response.json(
-          {success: true},
-          {headers: {
-              "Content-Type": "text/event-stream",
-              "Cache-Control": "no-cache",
-              "Connection": "keep-alive",
-              "Access-Control-Allow-Origin": origin,
-          }}
-        )
+          { success: true },
+          { headers: corsHeader(origin!) },
+        );
       } catch (err) {
         console.log("Dingdong endpoint caused exception. Error: ", err);
-        return serverError()
+        return Response.json(
+          { success: false },
+          { headers: corsHeader(origin!) },
+        );
       }
     }
   }
@@ -57,38 +84,59 @@ Deno.serve((req) => {
   return notFoundResponse();
 });
 
+function sseHeaders(origin: string): HeadersInit {
+  return {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+    ...corsHeader(origin),
+  };
+}
+
+function corsHeader(origin: string): HeadersInit {
+  return { "Access-Control-Allow-Origin": origin };
+}
+
+/** Avoid responding to headless, automated requests by
+requiring POST and body: { `isAuthorizedLol` }, and
+ensure host is allowed. */
 async function authenticate(req: Request) {
   if (req.method !== "POST") return false;
-  
-  const origin = req.headers.get("origin")
-  if (!origin) return false
+
+  const origin = req.headers.get("origin");
+  if (!origin) return false;
   if (!allowedOrigins.includes(origin)) return false;
-  
-  const { isAuthorizedLOL } =  await req.json();
-  if (!isAuthorizedLOL) return false;
+
+  // TODO: compare with an env var instead and get this out of the repo
+  try {
+    const { isAuthorizedLOL } = await req.json();
+    if (!isAuthorizedLOL) return false;
+  } catch {
+    return false;
+  }
 
   return true;
 }
 
 function getStream(req: Request) {
   const stream = new ReadableStream({
-          start(controller) {
-            const encoder = new TextEncoder();
-            const interval = setInterval(() => {
-              const data = `data: ${JSON.stringify({
-                is_bat_signal_busy: batSignal.isOn(),
-                is_someone_coming: batSignal.isSomeoneComing(),
-                })}\n\n`;
-              controller.enqueue(encoder.encode(data));
-            }, 1000);
+    start(controller) {
+      const encoder = new TextEncoder();
+      const interval = setInterval(() => {
+        const data = `data: ${JSON.stringify({
+          is_bat_signal_busy: batSignal.isOn(),
+          is_someone_coming: batSignal.isSomeoneComing(),
+        })}\n\n`;
+        controller.enqueue(encoder.encode(data));
+      }, SSE_MSG_INTERVAL_MS);
 
-            req.signal.addEventListener("abort", () => {
-              clearInterval(interval);
-              controller.close();
-            });
-          },
-        });
-  return stream
+      req.signal.addEventListener("abort", () => {
+        clearInterval(interval);
+        controller.close();
+      });
+    },
+  });
+  return stream;
 }
 
 function notFoundResponse() {
@@ -96,9 +144,9 @@ function notFoundResponse() {
 }
 
 function unauthorizedResponse() {
-  return new Response("Unauthorized", { status: 401 });
+  return new Response("Unathorized", { status: 401 });
 }
 
-function serverError() {
-  return new Response("Oops, server error", { status: 501})
+function serverErrorResponse() {
+  return new Response("Oops, server error", { status: 501 });
 }
